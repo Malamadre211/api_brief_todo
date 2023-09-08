6cf28fc9c00c0eefe69c3ef016d2d1e070210a16
malamadre211 <mariangelacurcioartist@gmail.com>
2023-09-08 12:07:45 +0200

    gosequelize

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello world');
+import express from "express"
+import "dotenv/config"
+
+const port = process.env.PORT as string
+const app = express();
+
+// const { Sequelize } = require('sequelize');
+// const sequelize = new Sequelize('postgres://user:mariangelacurcioartist@gmail.com:simplon/api_todo')
+
+// try {
+//     await sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+
+  app.get('/task/:valeur', (req, res) => {
+    const todo = req.params.valeur
+    res.send(`${todo}`)
+    console.log('todo')
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
