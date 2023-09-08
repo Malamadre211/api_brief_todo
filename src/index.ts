1245cb4fa6fc9198e1195ae22b71e6d3a15bffdb
malamadre211 <mariangelacurcioartist@gmail.com>
2023-09-08 15:18:27 +0200

    step bdd

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,50 @@
 import express from "express"
-import "dotenv/config"
+import dotenv from "dotenv"
+import { log } from "console";
 
-const port = process.env.PORT as string
 const app = express();
+dotenv.config();
+const port = process.env.PORT as string
+const database = process.env.database as string
+const username = process.env.username as string
+const password = process.env.password as string
 
-// const { Sequelize } = require('sequelize');
-// const sequelize = new Sequelize('postgres://user:mariangelacurcioartist@gmail.com:simplon/api_todo')
+console.log({database, username, password})
+ 
+import { Sequelize, DataTypes } from 'sequelize';
 
-// try {
-//     await sequelize.authenticate();
-//     console.log('Connection has been established successfully.');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
+// const sequelize = new Sequelize(database,username,password, {
+//     host: 'localhost',
+//     dialect: 'postgres'
+// });
+// const sequelize = new Sequelize(`postgres://${username}:${password}@localhost:5432/${database}`)
+// const sequelize = new Sequelize(database, 'postgres', password, {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     port:5432,
+// } );
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./db.sqlite"
+});
+
+const Todo = sequelize.define('todo', {
+    value: DataTypes.STRING,
+    status: DataTypes.BOOLEAN,
+});
+
+async function syncDB() {
+    try {
+        await sequelize.sync({ force: true });
+        console.log('Database connected');
+        Todo.create({ value : "tache 1", status: true})
+    } catch (error) {
+        console.error('error synching database', error);
+    }
+}
+syncDB();
+
 
   app.get('/task/:valeur', (req, res) => {
     const todo = req.params.valeur
